f01741d7e6ad3aac7b4007151f358847fd01939b
Palmer Dabbelt <palmer@dabbelt.com>
2011-06-13 13:12:31 -0400

    Print the PREFIX after bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,6 @@
 gcc `find src/pconfigure/ -iname "*.c"` -o "bin/pconfigure" || exit $?
 ./bin/pconfigure || exit $?
 make || exit $?
+prefix=`cat src/pconfigure/defaults.h  | grep PREFIX | cut -d ' ' -f 2 | cut -d '"' -f 2`
 echo "run 'make install' to install this to the system"
+echo -e "\tby default it is installed into $prefix"
