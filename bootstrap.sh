ee7b2ad82208b3dc818e92239dcee70e218b549b
Palmer Dabbelt <palmer@dabbelt.com>
2011-08-29 13:47:32 -0400

    Accidentally removed a newline

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 mkdir -p bin
-gcc `find src/pconfigure/ -iname "*.c"` -lclang -lLLVM-2.8 -L`llvm-config --libdir` -Wl,-R`llvm-config --libdir` -o "bin/pconfigure" || exit $?./bin/pconfigure || exit $?
+gcc `find src/pconfigure/ -iname "*.c"` -lclang -lLLVM-2.8 -L`llvm-config --libdir` -Wl,-R`llvm-config --libdir` -o "bin/pconfigure" || exit $?
+./bin/pconfigure || exit $?
 make || exit $?
 prefix=`cat src/pconfigure/defaults.h  | grep PREFIX | cut -d ' ' -f 2 | cut -d '"' -f 2`
 echo "run 'make install' to install this to the system"
