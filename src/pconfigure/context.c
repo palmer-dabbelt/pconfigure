5b8e6490b7a561c1b31857df264cd22a8fa07d45
Palmer Dabbelt <palmer@dabbelt.com>
2011-12-19 15:10:46 -0500

    Always mkdir before install

--- src/pconfigure/context.c
+++ src/pconfigure/context.c
@@ -143,6 +143,10 @@ int context_binary_destructor(struct context *c)
     /* There is an install/uninstall target */
     tmp = talloc_asprintf(context, "echo -e \"INS\\t%s\"", c->full_path);
     makefile_add_install(c->mf, tmp);
+    tmp = talloc_asprintf(context,
+			  "mkdir -p `dirname \"%s/%s\"` >& /dev/null || true",
+			  c->full_path, c->prefix);
+    makefile_add_install(c->mf, tmp);
     tmp = talloc_asprintf(context, "install -D %s `dirname \"%s/%s\"`",
                           c->full_path, c->prefix, c->full_path);
     makefile_add_install(c->mf, tmp);
