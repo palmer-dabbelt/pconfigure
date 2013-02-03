08b490b461acba2917a14b5ca046f0482fbc2026
Palmer Dabbelt <palmer@dabbelt.com>
2013-02-02 16:40:41 -0800

    Add pgcc-config
    The idea behind this is that sometimes you want to designate a GCC
    minor version, but you don't really care about the patch version.
    This lets you select a GCC that works for you.
    
    Now that I think about it, I should probably extend this a bit, but I
    guess it's OK for now...

---
+++ src/pgcc-config/main.bash
@@ -0,0 +1,20 @@
+if [[ "$1" == "" || "$2" == "" ]]
+then
+    echo "pgcc-config <program> <version"
+    exit 1
+fi
+
+program="$1"
+version="$2"
+
+for x in $(seq 9 0)
+do
+    if [[ "$(which "$program"-"$version"."$x" 2>/dev/null)" != "" ]]
+    then
+	echo "$program"-"$version"."$x"
+	exit 0
+    fi
+done
+
+echo "\${CC}"
+exit 1
