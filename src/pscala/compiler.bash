8f89b10e161105b42bfe08f3937f6984851cfa37
Palmer Dabbelt <palmer@dabbelt.com>
2013-03-20 22:36:31 -0700

    Use zinc instead of scalac if possible
    zinc is a new scalac replacement that support incremental compilation,
    which makes scala builds a whole lot less painful.

--- src/pscala/compiler.bash
+++ src/pscala/compiler.bash
@@ -28,22 +28,39 @@ then
 fi
 
 # Make a temporary working directory
-workdir=`mktemp -d -t pscalac.XXXXXX`
-trap "set +e; rm -rf $workdir; rm -f $outfile" EXIT
+#workdir=`mktemp -d -t pscalac.XXXXXX`
+#trap "set +e; rm -rf $workdir; rm -f $outfile" EXIT
+workdir="$(readlink -f $(pwd))"/"$(dirname "$outfile")"/pscalac-cache
+mkdir -p "$workdir"
 
 workjar="$workdir"/out.jar
 
 # Copy the Scala file into the current working directory.  We need to
 # make sure it's at the correct path, as otherwise I think it won't
 # get loaded correctly.
-scalac $sources -d "$workdir" $classpath
+if which zinc >& /dev/null
+then
+    strace -f -o "$workdir"/strace -e stat \
+	zinc -q -analysis-cache "$workdir"/zinc.cache \
+	$sources -d "$workdir" $classpath
+else
+    strace -f -o "$workdir"/strace -e stat \
+	scalac -make:changed $sources -d "$workdir" $classpath
+fi
+
+# There isn't a real way to get a list of clas files that Scala
+# outputs, so I guess at it using strace... :)
 cd "$workdir"
-find -iname "*.class" | xargs jar cf "$workjar"
+cat "$workdir"/strace | cut -d' ' -f 2- | sed 's/^ //g' \
+    | grep "^stat" | sed 's@stat("\(.*\)",.*@\1@' \
+    | grep "^$workdir" | (while read f; do test ! -f $f || echo $f; done; ) \
+    | sed "s@$workdir/*@@" | grep ".class$" \
+    | xargs jar cf "$workjar"
 cd - >& /dev/null
 
 # Finally, copy the file
 cp "$workjar" "$outfile"
 
 # Clean up
-rm -rf "$workdir"
-trap "true" EXIT
+#rm -rf "$workdir"
+#trap "true" EXIT
