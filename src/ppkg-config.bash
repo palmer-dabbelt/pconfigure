34857640d61266b67b4c2ecf8682dbd6ba23565e
Palmer Dabbelt <palmer@dabbelt.com>
2013-09-05 13:53:36 -0700

    Export sed commands in pkg-config
    This (hopefully) lets me define pkg-config requirements that change
    depending on if I'm bundling talloc or not.  I guess we'll see how it
    goes...

--- src/ppkg-config.bash
+++ src/ppkg-config.bash
@@ -1,29 +1,44 @@
 # Parses some special command-line options
 optional="false"
+pkgconfig="false"
+pkgtarget=""
+lastarg=""
 have=""
-while true
+args=""
+while [[ "$1" != "" ]]
 do
     if [[ "$1" == "--optional" ]]
     then
 	optional="true"
+    elif [[ "$1" == "--pkgconfig" ]]
+    then
+        pkgconfig="true"
+        pkgtarget="$lastarg"
+        args="$args --exists"
     elif [[ "$1" == "--have" ]]
     then
 	shift
 	have="$1"
     else
-	break
+	args="$args $1"
+        lastarg="$1"
     fi
     
     shift
 done
 
 # Actually runs pkg-config
-stdout=$(pkg-config $@ 2> /dev/null)
+stdout=$(pkg-config $args 2> /dev/null)
 retval="$?"
 
 # If --optional is given then don't panic
 if [[ "$retval" != "0" ]]
 then
+    if [[ "$pkgconfig" == "true" && "$have" != "" ]]
+    then
+        echo "s/@@HAVE_$have@@//g"
+    fi
+
     if [[ "$optional" == "true" ]]
     then
 	exit 0
@@ -35,7 +50,12 @@ fi
 # Checks for some special options
 if [[ "$have" != "" ]]
 then
-    stdout="$stdout -DHAVE_$have"
+    if [[ "$pkgconfig" == "true" ]]
+    then
+        stdout="$stdout s/@@HAVE_$have@@/$pkgtarget/g"
+    else
+        stdout="$stdout -DHAVE_$have"
+    fi
 fi
 
 # Outputs the results of pkg-config
