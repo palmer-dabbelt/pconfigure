palmer-dabbelt/pconfigure

commit a3d9a6ce6e58f9ae1372d7f1552e1c2859ef78b9

Add ptest, a testing framework
This is designed to be the simplest testing framework possible: it
simply runs a list of tests, reporting if they pass or fail.  The idea
is that I don't want to impose any sort of testing framework on the
user but instead just want a nice clean interface for running tests.

The nice part is that tests are just binaries, so they can by built by
pconfigure along with the rest of your binaries.  This is currently a
double-edged sword: it's pretty but it's really showing the
deficiencies in my BASH build support.  I guess that's a good excuse
to fix up my BASH support... :).

Configfiles/main
@@ -39,3 +39,6 @@ SOURCES	    += pscala/linker.bash
 
 BINARIES    += pnasm
 SOURCES	    += pnasm.bash
+
+BINARIES    += ptest
+SOURCES	    += ptest.bash

src/ptest.bash
@@ -1,0 +1,60 @@
+# Parse the commandline arguments
+test=""
+out=""
+bin=""
+while [[ "$1" != "" ]]
+do
+    if [[ "$1" == "--test" ]]
+    then
+        test="$2"
+        shift
+        shift
+    elif [[ "$1" == "--out" ]]
+    then
+        out="$2"
+        shift
+        shift
+    elif [[ "$1" == "--bin" ]]
+    then
+        bin="$2"
+        shift
+        shift
+    elif [[ "$1" == "--check" ]]
+    then
+        # This is a special case that's used for printing the
+        # success/failure of test cases
+        ret="$(tar -xOf "$2" ptest__return)"
+        if [[ "$ret" == "0" ]]
+        then
+            echo -e "PASS\t$(echo "$2" | cut -d'/' -f2-)"
+        elif [[ "$ret" == "1" ]]
+        then
+            echo -e "FAIL\t$(echo "$2" | cut -d'/' -f2-)"
+        else
+            echo -e "ERROR\t$(echo "$2" | cut -d'/' -f2-)"
+        fi
+
+        exit 0
+    else
+        echo "Unknown argument: $1"
+        exit 1
+    fi
+done
+
+out="$(readlink -f "$out")"
+bin="$(readlink -f "$bin")"
+test="$(readlink -f "$test")"
+
+# This is the regular path where we actually run a test case
+tmpdir=`mktemp -d`
+
+export PTEST_BINARY="$bin"
+export PTEST_TMPDIR="$tmpdir"
+
+"$test" >"$tmpdir"/ptest__stdout 2>"$tmpdir"/ptest__stderr
+echo "$?" >"$tmpdir"/ptest__return
+
+cd $tmpdir
+tar -c * > "$out"
+
+rm -rf $tmpdir

src/pconfigure/context.c
@@ -35,6 +35,7 @@ static int context_binary_destructor(struct context *c);
 static int context_library_destructor(struct context *c);
 static int context_header_destructor(struct context *c);
 static int context_source_destructor(struct context *c);
+static int context_test_destructor(struct context *c);
 
 struct context *context_new_defaults(struct clopts *o, void *context,
                                      struct makefile *mf,
@@ -45,11 +46,15 @@ struct context *context_new_defaults(struct clopts *o, void *context,
 
     c = talloc(context, struct context);
     c->type = CONTEXT_TYPE_DEFAULTS;
+    c->parent = NULL;
+    c->test_parent = NULL;
     c->bin_dir = talloc_strdup(c, "bin");
     c->lib_dir = talloc_strdup(c, "lib");
     c->hdr_dir = talloc_asprintf(c, "%sinclude", o->source_path);
     c->obj_dir = talloc_strdup(c, "obj");
     c->src_dir = talloc_asprintf(c, "%ssrc", o->source_path);
+    c->chk_dir = talloc_strdup(c, "check");
+    c->tst_dir = talloc_asprintf(c, "%stest", o->source_path);
     c->prefix = talloc_strdup(c, "/usr/local");
     c->compile_opts = stringlist_new(c);
     c->link_opts = stringlist_new(c);
@@ -69,11 +74,14 @@ struct context *context_new_binary(struct context *parent, void *context,
     c = talloc(context, struct context);
     c->type = CONTEXT_TYPE_BINARY;
     c->parent = c;
+    c->test_parent = NULL;
     c->bin_dir = talloc_reference(c, parent->bin_dir);
     c->lib_dir = talloc_reference(c, parent->lib_dir);
     c->hdr_dir = talloc_reference(c, parent->hdr_dir);
     c->obj_dir = talloc_reference(c, parent->obj_dir);
     c->src_dir = talloc_reference(c, parent->src_dir);
+    c->chk_dir = talloc_reference(c, parent->chk_dir);
+    c->tst_dir = talloc_reference(c, parent->tst_dir);
     c->prefix = talloc_reference(c, parent->prefix);
     c->compile_opts = stringlist_copy(parent->compile_opts, c);
     c->link_opts = stringlist_copy(parent->link_opts, c);
@@ -84,6 +92,7 @@ struct context *context_new_binary(struct context *parent, void *context,
     c->objects = stringlist_new(c);
     c->libraries = stringlist_new(c);
 
+    c->called_path = talloc_strdup(c, called_path);
     c->full_path = talloc_asprintf(c, "%s/%s", c->bin_dir, called_path);
     c->link_path = talloc_strdup(c, "");
     c->link_path_install = talloc_strdup(c, "");
@@ -258,12 +267,15 @@ struct context *context_new_library(struct context *parent, void *context,
     c = talloc(context, struct context);
     c->type = CONTEXT_TYPE_LIBRARY;
     c->parent = c;
+    c->test_parent = NULL;
     c->bin_dir = talloc_reference(c, parent->bin_dir);
     c->lib_dir = talloc_reference(c, parent->lib_dir);
     c->hdr_dir = talloc_reference(c, parent->hdr_dir);
     c->obj_dir = talloc_reference(c, parent->obj_dir);
     c->src_dir = talloc_reference(c, parent->src_dir);
     c->prefix = talloc_reference(c, parent->prefix);
+    c->chk_dir = talloc_reference(c, parent->chk_dir);
+    c->tst_dir = talloc_reference(c, parent->tst_dir);
     c->compile_opts = stringlist_copy(parent->compile_opts, c);
     c->link_opts = stringlist_copy(parent->link_opts, c);
     c->mf = talloc_reference(c, parent->mf);
@@ -273,6 +285,7 @@ struct context *context_new_library(struct context *parent, void *context,
     c->objects = stringlist_new(c);
     c->libraries = stringlist_new(c);
 
+    c->called_path = talloc_strdup(c, called_path);
     c->full_path = talloc_asprintf(c, "%s/%s", c->lib_dir, called_path);
     c->link_path = talloc_strdup(c, "");
     c->link_path_install = talloc_strdup(c, "");
@@ -397,11 +410,14 @@ struct context *context_new_header(struct context *parent, void *context,
     c = talloc(context, struct context);
     c->type = CONTEXT_TYPE_LIBRARY;
     c->parent = c;
+    c->test_parent = NULL;
     c->bin_dir = talloc_reference(c, parent->bin_dir);
     c->lib_dir = talloc_reference(c, parent->lib_dir);
     c->hdr_dir = talloc_reference(c, parent->hdr_dir);
     c->obj_dir = talloc_reference(c, parent->obj_dir);
     c->src_dir = talloc_reference(c, parent->src_dir);
+    c->chk_dir = talloc_reference(c, parent->chk_dir);
+    c->tst_dir = talloc_reference(c, parent->tst_dir);
     c->prefix = talloc_reference(c, parent->prefix);
     c->compile_opts = stringlist_copy(parent->compile_opts, c);
     c->link_opts = stringlist_copy(parent->link_opts, c);
@@ -412,6 +428,7 @@ struct context *context_new_header(struct context *parent, void *context,
     c->objects = stringlist_new(c);
     c->libraries = stringlist_new(c);
 
+    c->called_path = talloc_strdup(c, called_path);
     c->full_path = talloc_asprintf(c, "%s/%s", c->hdr_dir, called_path);
     c->link_path = talloc_strdup(c, "");
     c->link_path_install = talloc_strdup(c, "");
@@ -427,6 +444,11 @@ int context_header_destructor(struct context *c)
     void *context;
 
     context = talloc_new(c);
+
+    assert(c->type == CONTEXT_TYPE_HEADER);
+#ifdef DEBUG
+    fprintf(stderr, "context_header_destructor('%s')\n", c->full_path);
+#endif
 
     /* There is an install/uninstall target */
     tmp = talloc_asprintf(context, "echo -e \"INS\\t%s\"", c->full_path);
@@ -457,11 +479,14 @@ struct context *context_new_source(struct context *parent, void *context,
     c = talloc(context, struct context);
     c->type = CONTEXT_TYPE_SOURCE;
     c->parent = parent->parent;
+    c->test_parent = NULL;
     c->bin_dir = talloc_reference(c, parent->bin_dir);
     c->lib_dir = talloc_reference(c, parent->lib_dir);
     c->hdr_dir = talloc_reference(c, parent->hdr_dir);
     c->obj_dir = talloc_reference(c, parent->obj_dir);
     c->src_dir = talloc_reference(c, parent->src_dir);
+    c->chk_dir = talloc_reference(c, parent->chk_dir);
+    c->tst_dir = talloc_reference(c, parent->tst_dir);
     c->prefix = talloc_reference(c, parent->prefix);
     c->compile_opts = stringlist_copy(parent->compile_opts, c);
     c->link_opts = stringlist_copy(parent->link_opts, c);
@@ -472,6 +497,7 @@ struct context *context_new_source(struct context *parent, void *context,
     c->objects = stringlist_new(c);
     c->libraries = stringlist_new(c);
 
+    c->called_path = talloc_strdup(c, called_path);
     c->full_path = talloc_asprintf(c, "%s/%s", c->src_dir, called_path);
     c->link_path = talloc_strdup(c, "");
     c->link_path_install = talloc_strdup(c, "");
@@ -489,11 +515,14 @@ struct context *context_new_fullsrc(struct context *parent, void *context,
     c = talloc(context, struct context);
     c->type = CONTEXT_TYPE_SOURCE;
     c->parent = parent->parent;
+    c->test_parent = NULL;
     c->bin_dir = talloc_reference(c, parent->bin_dir);
     c->lib_dir = talloc_reference(c, parent->lib_dir);
     c->hdr_dir = talloc_reference(c, parent->hdr_dir);
     c->obj_dir = talloc_reference(c, parent->obj_dir);
     c->src_dir = talloc_reference(c, parent->src_dir);
+    c->chk_dir = talloc_reference(c, parent->chk_dir);
+    c->tst_dir = talloc_reference(c, parent->tst_dir);
     c->prefix = talloc_reference(c, parent->prefix);
     c->compile_opts = stringlist_copy(parent->compile_opts, c);
     c->link_opts = stringlist_copy(parent->link_opts, c);
@@ -625,6 +654,146 @@ int context_source_destructor(struct context *c)
     language_quirks(l, c, c->mf);
 
     /* Everything succeeded! */
+    TALLOC_FREE(context);
+    return 0;
+}
+
+struct context *context_new_test(struct context *parent, void *context,
+                                 const char *called_path)
+{
+    struct context *c;
+
+    c = talloc(context, struct context);
+    c->type = CONTEXT_TYPE_TEST;
+    c->parent = c;
+    c->test_parent = parent;
+    c->bin_dir = talloc_reference(c, parent->chk_dir);
+    c->lib_dir = talloc_reference(c, parent->lib_dir);
+    c->hdr_dir = talloc_reference(c, parent->hdr_dir);
+    c->obj_dir = talloc_reference(c, parent->obj_dir);
+    c->src_dir = talloc_asprintf(c, "%s/%s", parent->tst_dir,
+                                 parent->called_path);
+    c->chk_dir = talloc_reference(c, parent->chk_dir);
+    c->tst_dir = talloc_reference(c, parent->tst_dir);
+    c->prefix = talloc_reference(c, parent->prefix);
+    c->compile_opts = stringlist_copy(parent->compile_opts, c);
+    c->link_opts = stringlist_copy(parent->link_opts, c);
+    c->mf = talloc_reference(c, parent->mf);
+    c->ll = talloc_reference(c, parent->ll);
+    c->s = parent->s;
+    c->language = NULL;
+    c->objects = stringlist_new(c);
+    c->libraries = stringlist_new(c);
+
+    c->called_path = talloc_strdup(c, called_path);
+    c->full_path = talloc_asprintf(c, "%s/%s/%s", c->bin_dir,
+                                   parent->called_path, called_path);
+    c->link_path = talloc_strdup(c, "");
+    c->link_path_install = talloc_strdup(c, "");
+
+    talloc_set_destructor(c, &context_test_destructor);
+
+    return c;
+}
+
+int context_test_destructor(struct context *c)
+{
+    struct language *l;
+    void *context;
+    const char *hash_langlinkopts, *hash_linkopts, *hash_objs;
+
+    assert(c->type == CONTEXT_TYPE_TEST);
+#ifdef DEBUG
+    fprintf(stderr, "context_test_destructor('%s')\n", c->full_path);
+#endif
+
+    l = c->language;
+    assert(l != NULL);
+
+    context = talloc_new(NULL);
+
+    /* *INDENT-OFF* */
+#ifdef DEBUG
+    stringlist_each(c->objects,
+		    lambda(int, (const char *obj),
+			   {
+			       fprintf(stderr, "obj: %s\n", obj);
+			       return 0;
+			   }
+			));
+#endif
+    /* *INDENT-ON* */
+
+    hash_langlinkopts = stringlist_hashcode(c->language->link_opts, context);
+    hash_linkopts = stringlist_hashcode(c->link_opts, context);
+    hash_objs = stringlist_hashcode(c->objects, context);
+    talloc_unlink(c, (char *)c->link_path);
+    c->link_path = talloc_asprintf(c, "%s/%s/%s-%s-%s.bin",
+                                   c->obj_dir, c->full_path,
+                                   hash_langlinkopts, hash_linkopts,
+                                   hash_objs);
+    c->link_path_install = talloc_asprintf(c, "%s/%s/%s-%s-%s.ins/%s",
+                                           c->obj_dir, c->full_path,
+                                           hash_langlinkopts, hash_linkopts,
+                                           hash_objs, c->full_path);
+
+    makefile_add_targets(c->mf, c->full_path);
+    makefile_add_check(c->mf, c->full_path);
+
+    /* Run the test, producing a tarball with the results. */
+    makefile_create_target(c->mf, c->full_path);
+    makefile_start_deps(c->mf);
+    makefile_add_dep(c->mf, "%s", c->test_parent->full_path);
+    makefile_add_dep(c->mf, "%s", c->link_path);
+    makefile_end_deps(c->mf);
+    makefile_start_cmds(c->mf);
+    makefile_nam_cmd(c->mf, "echo \"TEST\t%s\"",
+                     c->full_path + strlen(c->chk_dir) + 1);
+    makefile_add_cmd(c->mf, "mkdir -p `dirname %s`", c->full_path);
+    makefile_add_cmd(c->mf, "ptest --test %s --out %s --bin %s",
+                     c->link_path, c->full_path, c->test_parent->full_path);
+    makefile_end_cmds(c->mf);
+
+    /* Does the actual linking with hashes of the arguments */
+    makefile_create_target(c->mf, c->link_path);
+
+    makefile_start_deps(c->mf);
+    /* *INDENT-OFF* */
+    stringlist_each(c->objects, lambda(int, (const char *obj),
+			       {
+				   makefile_add_dep(c->mf, "%s", obj);
+				   return 0;
+			       }
+		      ));
+    stringlist_each(c->libraries, lambda(int, (const char *lib),
+			       {
+				   makefile_add_dep(c->mf, "%s/lib%s.so",
+						    c->lib_dir, lib);
+				   return 0;
+			       }
+		      ));
+    /* *INDENT-ON* */
+    makefile_end_deps(c->mf);
+
+    makefile_start_cmds(c->mf);
+    /* *INDENT-OFF* */
+    language_link(l, c, lambda(void, (bool nam,
+				      const char *format, ...),
+			       {
+				   va_list args;
+				   va_start(args, NULL);
+				   if (nam == true)
+				       makefile_vnam_cmd(c->mf, format, args);
+				   else
+				       makefile_vadd_cmd(c->mf, format, args);
+				   va_end(args);
+			       }
+		      ), false);
+    /* *INDENT-ON* */
+    makefile_end_cmds(c->mf);
+
+    makefile_add_distclean(c->mf, c->chk_dir);
+
     TALLOC_FREE(context);
     return 0;
 }

src/pconfigure/context.h
@@ -38,7 +38,8 @@ enum context_type
     CONTEXT_TYPE_BINARY,
     CONTEXT_TYPE_LIBRARY,
     CONTEXT_TYPE_HEADER,
-    CONTEXT_TYPE_SOURCE
+    CONTEXT_TYPE_SOURCE,
+    CONTEXT_TYPE_TEST,
 };
 
 struct context
@@ -50,6 +51,9 @@ struct context
      * ensure that dependencies are not added twice. */
     struct context *parent;
 
+    /* This is a special parent used for tests. */
+    struct context *test_parent;
+
     /* These should be the same globally */
     struct makefile *mf;
     struct languagelist *ll;
@@ -57,6 +61,9 @@ struct context
     /* Each context has a language associated with it, the reason for this is
      * to assure that all sources are compatible with one another. */
     struct language *language;
+
+    /* This is the path that this was called with */
+    char *called_path;
 
     /* This is the full path (include the bin/ or src/) of this context */
     char *full_path;
@@ -73,6 +80,8 @@ struct context
     char *hdr_dir;
     char *obj_dir;
     char *src_dir;
+    char *chk_dir;
+    char *tst_dir;
     char *prefix;
 
     /* Options specific to this context */
@@ -108,6 +117,9 @@ extern struct context *context_new_source(struct context *parent,
 extern struct context *context_new_fullsrc(struct context *parent,
                                            void *context,
                                            const char *full_path);
+extern struct context *context_new_test(struct context *parent,
+                                        void *context,
+                                        const char *called_path);
 
 extern int context_set_prefix(struct context *c, char *opt);
 

src/pconfigure/contextstack.c
@@ -259,6 +259,37 @@ void contextstack_push_fullsrc(struct contextstack *s, const char *full_path)
     s->head = cur;
 }
 
+void contextstack_push_test(struct contextstack *s, const char *called_path)
+{
+    struct contextstack_node *cur;
+    struct context *c;
+
+    if (s == NULL)
+    {
+        abort();
+        return;
+    }
+    if (called_path == NULL)
+        return;
+
+    cur = talloc(s, struct contextstack_node);
+    if (cur == NULL)
+        abort();
+
+    /* Creates a new context, based on the current context */
+    c = context_new_test(s->head->data, cur, called_path);
+    if (c == NULL)
+    {
+        TALLOC_FREE(cur);
+        abort();
+    }
+
+    /* This context is the new head. */
+    cur->data = c;
+    cur->next = s->head;
+    s->head = cur;
+}
+
 void contextstack_set_default_lib_dir(struct contextstack *s,
                                       const char *path)
 {

src/pconfigure/contextstack.h
@@ -68,6 +68,8 @@ extern void contextstack_push_source(struct contextstack *s,
                                      const char *called_path);
 extern void contextstack_push_fullsrc(struct contextstack *s,
                                       const char *full_path);
+extern void contextstack_push_test(struct contextstack *s,
+                                   const char *called_path);
 
 /* Sets some default options. */
 extern void contextstack_set_default_lib_dir(struct contextstack *s,

src/pconfigure/main.c
@@ -60,6 +60,8 @@ static int parsefunc_sources(const char *op, const char *right);
 static int parsefunc_compiler(const char *op, const char *right);
 static int parsefunc_linker(const char *op, const char *right);
 static int parsefunc_libdir(const char *op, const char *right);
+static int parsefunc_tests(const char *op, const char *right);
+static int parsefunc_testsrc(const char *op, const char *right);
 
 /* This is global data to avoid having really long parsefunc_* function calls */
 static struct clopts *o;
@@ -370,6 +372,10 @@ int parse_select(const char *left, const char *op, char *right)
         return parsefunc_linker(op, right);
     if (strcmp(left, "LIBDIR") == 0)
         return parsefunc_libdir(op, right);
+    if (strcmp(left, "TESTS") == 0)
+        return parsefunc_tests(op, right);
+    if (strcmp(left, "TESTSRC") == 0)
+        return parsefunc_testsrc(op, right);
 
     return -2;
 }
@@ -839,4 +845,58 @@ int parsefunc_libdir(const char *op, const char *right)
 
     contextstack_set_default_lib_dir(s, right);
     return 0;
+}
+
+int parsefunc_tests(const char *op, const char *right)
+{
+    if (strcmp(op, "+=") != 0)
+    {
+        fprintf(stderr, "We only support += for TESTS\n");
+        return -1;
+    }
+
+    /* We actually want to clear _almost_ everything on the stack,
+     * just everything up to and including the binary. */
+    while (!contextstack_isempty(s))
+    {
+        void *context;
+        enum context_type type;
+
+        /* Peek at the head of the stack, grabbing its type */
+        context = talloc_new(NULL);
+        type = contextstack_peek(s, context)->type;
+        TALLOC_FREE(context);
+
+        /* We don't want to push that last binary because it's needed
+         * by the test cases in order to run against said binary. */
+        if (type == CONTEXT_TYPE_BINARY)
+            goto push_test;
+
+        /* Actually pop the stack here */
+        context = talloc_new(NULL);
+        contextstack_pop(s, context);
+        TALLOC_FREE(context);
+    }
+
+  push_test:
+    /* Adds a binary to the stack, using the default compile options.  There is
+     * no need for this to  */
+    contextstack_push_test(s, right);
+    return 0;
+}
+
+int parsefunc_testsrc(const char *op, const char *right)
+{
+    int err;
+
+    if (strcmp(op, "+=") != 0)
+    {
+        fprintf(stderr, "We only support += for TESTS and SOURCES\n");
+        return -1;
+    }
+
+    if ((err = parsefunc_tests(op, right)) != 0)
+        return err;
+
+    return parsefunc_sources(op, right);
 }

src/pconfigure/makefile.c
@@ -43,6 +43,7 @@ struct makefile *makefile_new(struct clopts *o)
     m->file = fopen("Makefile", "w");
     m->targets_all = stringlist_new(m);
     m->targets_all_install = stringlist_new(m);
+    m->targets_check = stringlist_new(m);
     m->targets_clean = stringlist_new(m);
     m->targets_cleancache = stringlist_new(m);
     m->targets_distclean = stringlist_new(m);
@@ -54,7 +55,7 @@ struct makefile *makefile_new(struct clopts *o)
     fprintf(m->file, "SHELL=/bin/bash\n");
     fprintf(m->file,
             ".PHONY: all pconfigure__all clean cleancache distclean "
-            "install uninstall all_install" "\n");
+            "install uninstall all_install " "\n");
     fprintf(m->file, ".SUFFIXES:\n");
     fprintf(m->file, "all: pconfigure__all\n");
     fprintf(m->file, "\n");
@@ -84,6 +85,12 @@ void makefile_add_all_install(struct makefile *m, const char *name)
 {
     if (stringlist_include(m->targets_all, name) == false)
         stringlist_add(m->targets_all_install, name);
+}
+
+void makefile_add_check(struct makefile *m, const char *name)
+{
+    if (stringlist_include(m->targets_check, name) == false)
+        stringlist_add(m->targets_check, name);
 }
 
 void makefile_add_clean(struct makefile *m, const char *name)
@@ -292,6 +299,27 @@ int mf_destructor(struct makefile *m)
     }
     makefile_end_deps(m);
     makefile_start_cmds(m);
+    makefile_end_cmds(m);
+
+    /* Allows us to run the tests */
+    makefile_create_target(m, "check");
+
+    makefile_start_deps(m);
+    cur = stringlist_start(m->targets_check);
+    while (stringlist_notend(cur))
+    {
+        makefile_add_dep(m, stringlist_data(cur));
+        cur = stringlist_next(cur);
+    }
+    makefile_end_deps(m);
+
+    makefile_start_cmds(m);
+    cur = stringlist_start(m->targets_check);
+    while (stringlist_notend(cur))
+    {
+        makefile_add_cmd(m, "@ptest --check %s", stringlist_data(cur));
+        cur = stringlist_next(cur);
+    }
     makefile_end_cmds(m);
 
     /* Cleans the cache */

commit f2b060b0584f8012875896b3b2ea752e9297422a

pconfigure: Don't hand a test the flags of the make that ran it
make puts its own command line in MAKEFLAGS and exports it to every
recipe, so a test that builds anything ran its own make with whatever
the outer one was given.  Most of the time that is harmless.  Under
"make -B" it is not: the order to rebuild everything arrives inside the
test, and the eight tests that exist to watch what gets rebuilt -- what
a touched file drags with it, what a subproject rebuilds, what a
vendored dependency does -- all see everything rebuild and all fail.

That reads as eight real failures, which is the expensive part.  A
clean "make -B report" is a reasonable thing to ask for and it has been
answering with a wall of red that says nothing about the tree.

Nothing about the outer make's command line means anything to a test,
so unset it.  "-n" and "-k" would be no better than "-B" if they ever
turned up.  The test checks that the variable is gone rather than
empty, because make sets it either way.

Fixes: a3d9a6ce6e58 ("Add ptest, a testing framework")
Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

Configfiles/main
@@ -207,6 +207,7 @@ TESTSRC     += testdeps-outside.bash
 TESTSRC     += deptests.bash
 TESTSRC     += deptests-bad.bash
 TESTSRC     += ptest-check-dirs.bash
+TESTSRC     += makeflags.bash
 TESTSRC     += cmdline-args.bash
 TESTSRC     += strict.bash
 TESTSRC     += strict-opts-target.bash

src/ptest.bash
@@ -281,6 +281,17 @@ fi
 tmpdir=`mktemp -d -t ptest-wrapper.XXXXXXXXXX`
 trap "rm -rf $tmpdir" EXIT
 
+# A test that builds anything runs a make of its own, and it must not
+# inherit this one's flags.  make puts them in the environment of every
+# recipe it runs, so a "make -B" out here arrives in there as an order
+# to rebuild everything -- which is the one thing the tests that check
+# what gets rebuilt are looking at, and they fail for a reason that has
+# nothing to do with what they test.  "-n" and "-k" are no better.
+# Nothing about the outer make's command line means anything to a test.
+unset MAKEFLAGS
+unset MFLAGS
+unset MAKELEVEL
+
 export PTEST_BINARY="$bin"
 export PTEST_SRCDIR="$srcdir"
 export PTEST_CHECKDIR="$checkdir"

test/pconfigure/makeflags.bash
@@ -1,0 +1,2 @@
+#include "harness.bash"
+H4sIAAAAAAACA+2az2/bNhTHvR113XGXV7eHDWsV/SAlq0MOTuAlRtM2tZMB2422aZuIRBkU5aQoctjf0D+v6L9SlJLbFHHbNO4iCYXf5yKKICnS9PfxPZLH7OKQswlXO+NcKS71RKjWHeM4TkgplM9g9XQ8snqWadcBl3rEDzyXOBQcl1JKWnDRqoE800yZrixYnPAbhm6KTac3D9IAV88fBN+BRIuE77phJzSzQ53IDiihYUQjatEQjvp73cH+Yf/vnn3BtFb2OE1stljE3F6odMklk2O+233R784GxyP/36ejnbNTi0QwNJWO/rmp0k8/t/775c3b17++61ktpBHsneq/8S39F3q5rn83IE4LKOq/hvk/vloB9lM5FbOpiPmdz39AyGb2P/TQ/jdn/11KoqgTov3fAvtflepvr38j+jX9m5TfAgf1XzlH3WcHp92D3hD+2IURy+bWtZyxZe31n3UH/dWr5pm2hs9PB/vm/WOGPbZOesOT4WC/yEnYGZ/GbJbZZWuosB9n/S8mswn/7/P1n1AnxPW/sfWfdKjne66P6/8W6L9QfbVB4Obxnxe4IcZ/Tdh/e5bGk5r9P88J1+M/k0b7Xwde9JX9PxJ6aP63xf5Xofrb6/9z+++TYv8f47967X+mxo2s/1/w/30X/f8m9/86ZipcPP/ZBvtvVF/xGdB3+P/EQ/+/xvlfiwHGdz7/m/r/1MPznwb9fzfyjQbR/G+L/a9C9Rv4/7677v9Tgv5/LQipIWFC/vY7vALFda4kOH/CJSpym+L/ag+Bvhn/fWH/L8T7fw2u/x6JiI/XP7ZH/5UeAm0e//k0DDD+q3v+Py0C1y9xVB7/re//eY7vu2j/m9v/I14U+h0MALfL/t+t6m+v/8Bbt/+BH1CM/+rgPpzMeXlpD/ScaVBMwqL4N8Ai1yB0Bum5hPJfAUKaMhy4XAqVyoRL/RCYnACz7pcXAWHOMpApjPJMSJ5lkHEu5KyolDwGBu3yM4/22pAvikzF4TzNY9OAUmLJTStlnmnEdCJVZiECnZqgdJQLU4gvuXqp56ZBGz50MZcZNz0s2n8IWQrnZgRFX+YigzhNzzKYpgpEWQKWTAk2ijmMyk7NUsnNaItuFCOXH7J5stAvbUtMVyNqP3j1tPuk99dR92D4qPzcZRvu7UK7TLctU11aYODjeQrtq7IQczPWifnJzAiKrxetPYYHVwXaq1oXQoNrTYVllUkH7SCCIAiCIAiCIAiCIAiCIAiCIAjyv3gP+UdPdQBQAAA=