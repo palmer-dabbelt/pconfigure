palmer-dabbelt/pconfigure

commit 024f77fa18f4b993289d52937f0df838fd0454fb

Implement libpinlcude++ in C++

Configfiles/main
@@ -134,7 +134,6 @@ SOURCES     += libpinclude/pinclude++.pc
 # quite pconfigure.
 LIBRARIES   += libpconfigure.so
 DEPLIBS     += pinclude
-DEPLIBS     += pinclude
 DEPLIBS     += pinclude++
 CONFIG      += libpconfigure_sources
 CONFIG      += libpconfigure_headers
@@ -152,7 +151,6 @@ CONFIG      += libmakefile_sources
 # into the 21st century!
 BINARIES    += pconfigure
 AUTODEPS     = false
-DEPLIBS     += pinclude
 DEPLIBS     += pinclude++
 DEPLIBS     += pconfigure
 DEPLIBS     += makefile

src/libpinclude/library.c++
@@ -19,16 +19,11 @@
  */
 
 #include <pinclude.h++>
-#include <pinclude.h>
-
-class pinclude_args {
-public:
-    const std::function<int(std::string)> func;
-    pinclude_args(const std::function<int(std::string)> _func)
-    : func(_func)
-    {} 
-};
-static int pinclude_callback(const char *path, void *args_uncase);
+#include <fstream>
+#include <stack>
+#include <iostream>
+#include <fcntl.h>
+#include <unistd.h>
 
 int pinclude::list(std::string filename,
                    std::vector<std::string> include_dirs,
@@ -36,36 +31,539 @@ int pinclude::list(std::string filename,
                    std::function<int(std::string)> callback,
                    bool skip_missing_files)
 {
-    size_t defines_i = 0;
-    const char **defines_a = new const char*[defines.size() + 1];
-    size_t include_dirs_i = 0;
-    const char **include_dirs_a = new const char*[include_dirs.size() + 1];
-
-    for (const auto& include_dir: include_dirs)
-        include_dirs_a[include_dirs_i++] = include_dir.c_str();
-    include_dirs_a[include_dirs_i] = nullptr;
-
+    std::unordered_set<std::string> define_set;
     for (const auto& define: defines)
-        defines_a[defines_i++] = define.c_str();
-    defines_a[defines_i] = nullptr;
+        define_set.insert(define);
 
-    auto args = pinclude_args(callback);
-    auto out = pinclude_list(filename.c_str(),
-                             &pinclude_callback,
-                             &args,
-                             &include_dirs_a[0],
-                             &defines_a[0],
-                             skip_missing_files ? 1 : 0);
-
-    delete[] defines_a;
-    delete[] include_dirs_a;
-
-    return out;
- 
+    return list(filename,
+                include_dirs,
+                define_set,
+                callback,
+                skip_missing_files);
 }
 
-int pinclude_callback(const char *path, void *args_uncast)
+enum class state {
+    OUTPUT,
+    ELSE,
+};
+
+template<typename T> class option {
+private:
+    bool _valid;
+    T _data;
+
+public:
+    option(void)
+    : _valid(false)
+    {}
+
+    option(const T& data)
+    : _valid(true),
+      _data(data)
+    {}
+
+public:
+    bool valid(void) const { return _valid; }
+    const T& data(void) const { return _data; }
+};
+
+static void check_line(
+    const std::string& line,
+    const std::string& pp, 
+    const std::function<void(std::string)> on_match
+);
+
+static bool resolve_pp_function(
+    const std::string& f,
+    const std::unordered_set<std::string>& defines
+);
+
+static bool resolve_pp_function(
+    const std::vector<std::string>& f,
+    const std::unordered_set<std::string>& defines
+);
+
+static bool resolve_pp_function(
+    std::vector<std::string>::const_iterator begin,
+    std::vector<std::string>::const_iterator end,
+    const std::unordered_set<std::string>& defines
+);
+
+static bool resolve_pp_defined(
+    std::vector<std::string>::const_iterator begin,
+    std::vector<std::string>::const_iterator end,
+    const std::unordered_set<std::string>& defines
+);
+
+template<typename T>
+static std::vector<T> cat(const std::vector<T>& a, const std::vector<T>& b)
 {
-    auto args = (class pinclude_args*)args_uncast;
-    return args->func(path);
+    auto out = std::vector<T>();
+    out.insert(out.end(), a.begin(), a.end());
+    out.insert(out.end(), b.begin(), b.end());
+    return out;
+}
+
+static
+int list_overwrite_defines(std::string filename,
+                           std::vector<std::string> include_dirs_without_cwd,
+                           std::unordered_set<std::string>& defines,
+                           std::function<int(std::string)> callback,
+                           bool skip_missing_files);
+
+int pinclude::list(std::string filename,
+                   std::vector<std::string> include_dirs_without_cwd,
+                   std::unordered_set<std::string> defines,
+                   std::function<int(std::string)> callback,
+                   bool skip_missing_files)
+{
+    return list_overwrite_defines(
+        filename,
+        include_dirs_without_cwd,
+        defines,
+        callback,
+        skip_missing_files
+    );
+}
+
+int list_overwrite_defines(std::string filename,
+                           std::vector<std::string> include_dirs,
+                           std::unordered_set<std::string>& defines,
+                           std::function<int(std::string)> callback,
+                           bool skip_missing_files)
+{
+    std::ifstream file(filename);
+    std::string line;
+
+    std::stack<state> state_stack;
+    state_stack.push(state::OUTPUT);
+
+    static const auto split = [](std::string str, std::string delim) {
+        std::vector<size_t> slashes;
+        {
+            size_t i = 0;
+            while (i < str.size() && i != std::string::npos) {
+                slashes.push_back(i == 0 ? 0 : i + 1);
+                i = str.find_first_of(delim, i + 1);
+            }
+            slashes.push_back(str.size() + 1);
+        }
+
+        std::vector<std::string> out;
+        for (size_t i = 0; i < slashes.size() - 1; ++i)
+            out.push_back(str.substr(slashes[i], slashes[i+1] - slashes[i] - 1));
+        return out;
+    };
+
+    static const auto strip_dd = [](std::string in) {
+        auto insplit = split(in, "/");
+        auto leading_dotdot = [&]() {
+            for (size_t i = 0; i < insplit.size(); ++i)
+                if (insplit[i] != "..")
+                    return i;
+
+            std::cerr << "path is all ..\n";
+            abort();
+            return (size_t)-1;
+        }();
+
+        std::vector<std::string> out;
+        for (size_t i = leading_dotdot; i < insplit.size(); ++i) {
+            if (insplit[i] == "..")
+                out.erase(out.end() - 1);
+            else
+                out.push_back(insplit[i]);
+        }
+
+        std::string ostr = leading_dotdot == 0 ? "" : "..";
+        for (size_t i = 1; i < leading_dotdot; ++i)
+            ostr = ostr + "/..";
+        ostr = ostr + ((ostr.size() == 0) ? "" : "/") + ((out.size() == 0) ? "" : out[0]);
+        for (size_t i = 1; i < out.size(); ++i)
+            ostr = ostr + "/" + out[i];
+
+        return ostr;
+    };
+
+    static const auto get_relative_cwd = [](std::string filename_noncanon) {
+        auto filename = strip_dd(filename_noncanon);
+
+        auto last_slash = filename.find_last_of("/");
+        if (last_slash == std::string::npos)
+            return std::string("");
+
+        return filename.substr(0, last_slash);
+    };
+
+    static const auto next_logical_line = [](std::ifstream& file, std::string& out) {
+        std::string line;
+        if (!std::getline(file, line))
+            return false;
+        out = line;
+
+        while (line[line.size() - 1] == '\\') {
+            if (!std::getline(file, line))
+                return true;
+
+            out = out.substr(0, out.size() - 1) + line;
+        }
+
+        return true;
+    };
+
+    while (next_logical_line(file, line)) {
+        check_line(line, "if", [&](std::string rest) {
+            auto resolved = resolve_pp_function(rest, defines);
+            state_stack.push(resolved ? state::OUTPUT : state::ELSE);
+        });
+
+        check_line(line, "ifdef", [&](std::string rest) {
+            auto resolved = resolve_pp_function("defined(" + rest + ")", defines);
+            state_stack.push(resolved ? state::OUTPUT : state::ELSE);
+        });
+
+        check_line(line, "ifndef", [&](std::string rest) {
+            auto resolved = resolve_pp_function("!defined(" + rest + ")", defines);
+            state_stack.push(resolved ? state::OUTPUT : state::ELSE);
+        });
+
+        check_line(line, "define", [&](std::string rest) {
+            auto after = [&]() {
+                for (size_t i = 0; i < rest.size(); ++i)
+                    if (isspace(rest[i]) || rest[i] == '(')
+                        return i;
+                return std::string::npos;
+            }();
+
+            auto define = (after == std::string::npos)
+                ? rest
+                : (rest.substr(0, after));
+            defines.insert(define);
+        });
+
+        check_line(line, "else", [&](std::string rest) {
+            if (rest != "") {
+                std::cerr << "There shouldn't be anything after an else\n";
+                abort();
+            }
+
+            if (state_stack.size() == 0) {
+                std::cerr << "else without if\n";
+                abort();
+            }
+
+            auto ss = state_stack.top();
+            state_stack.pop();
+            switch (ss) {
+            case state::OUTPUT:
+                state_stack.push(state::ELSE);
+                break;
+            case state::ELSE:
+                state_stack.push(state::OUTPUT);
+                break;
+            }
+        });
+
+        check_line(line, "endif", [&](std::string rest) {
+            if (rest != "") {
+                std::cerr << "There shouldn't be anything after an endif\n";
+                abort();
+            }
+
+            if (state_stack.size() == 0) {
+                std::cerr << "endif without a cooresponding open\n";
+                abort();
+            }
+
+            if (state_stack.size() == 0) {
+                std::cerr << "endif without if\n";
+                abort();
+            }
+            state_stack.pop();
+        });
+
+        check_line(line, "include", [&](std::string rest) {
+            if (state_stack.top() != state::OUTPUT)
+                return;
+
+            auto rest_path = [&]() {
+                if (rest[0] == '<')
+                    return rest.substr(1, rest.size() - 2);
+                if (rest[0] == '"')
+                    return rest.substr(1, rest.size() - 2);
+
+                std::cerr << "Unable to parse line " << line << "\n";
+                std::cerr << "  Unknown include format, expected < or \"" << "\n";
+                abort();
+            }();
+
+            auto full_path = [&]() {
+                auto cwd_relative = get_relative_cwd(filename) + "/" + rest_path;
+                if (access(cwd_relative.c_str(), R_OK) == 0)
+                    return option<std::string>(strip_dd(cwd_relative));
+
+                for (const auto& dir: include_dirs) {
+                    auto check = dir + "/" + rest_path;
+                    if (access(check.c_str(), R_OK) == 0)
+                        return option<std::string>(strip_dd(check));
+                }
+
+                return option<std::string>();
+            }();
+
+            if (skip_missing_files == false && full_path.valid() == false) {
+                std::cerr << "Unable to open file: " << rest_path << "\n";
+                abort();
+            }
+
+            if (full_path.valid() == true) {
+                auto fout = callback(full_path.data());
+                if (fout != 0) {
+                    std::cerr << "Early out no longer supported in pinclude::list\n";
+                    abort();
+                }
+
+                auto rout = list_overwrite_defines(
+                    full_path.data(),
+                    include_dirs,
+                    defines,
+                    callback,
+                    skip_missing_files
+                );
+                if (rout != 0) {
+                    std::cerr << "Early out no longer supported in pinclude::list\n";
+                    abort();
+                }
+            }
+        });
+    }
+    return 0;
+}
+
+static void check_line(const std::string& line, const std::string& pp, const std::function<void(std::string)> on_match)
+{
+    size_t i = 0;
+
+    /* The first non-whitespace character must be a # */
+    while (i < line.size() && isspace(line[i]))
+        i++;
+    if (line[i] != '#')
+        return;
+
+    /* After the # there can be any number of spaces, so we just skip them. */
+    i++;
+    while (i < line.size() && isspace(line[i]))
+        i++;
+
+    /* Sometimes this exactly matches the proprocessor declaration. */
+    if (line.substr(i) == pp)
+        return on_match("");
+
+    /* Check to see if there's trailing space-like things after the
+     * declaration. */
+    if (line.size() < pp.size())
+        return;
+    if (line.substr(i, pp.size()) != pp)
+        return;
+    if (!isspace(line[i + pp.size()]))
+        return;
+
+    /* Here we strip the extra whitespace before after the directive before
+     * calling the given function on matches. */
+    i += pp.size();
+    while (i < line.size() && isspace(line[i]))
+        i++;
+
+    /* Checks to see if there's any comments, and strip those. */
+    auto comment_i = i;
+    while (comment_i < line.size()) {
+        if (line.substr(comment_i, 2) == "//")
+            break;
+        if (line.substr(comment_i, 2) == "/*")
+            break;
+        comment_i++;
+    }
+
+    return on_match(line.substr(i, comment_i - i));
+}
+
+static bool resolve_pp_function(
+    const std::string& function,
+    const std::unordered_set<std::string>& defines)
+{
+    static const auto token_terminators = std::vector<std::string>{
+        "(", ")", "&&", "||", "!", "defined", " "
+    };
+    static const auto terminates = [&](std::string str) {
+        for (const auto& term: token_terminators) {
+            if (str.substr(0, term.size()) == term)
+                return term;
+        }
+        return std::string("");
+    };
+
+    static const auto is_all_white = [](std::string str) {
+        for (const auto& c: str)
+            if (!isspace(c))
+                return false;
+        return true;
+    };
+
+    std::vector<std::string> tokenized;
+    {
+        size_t token_begin = 0;
+        size_t token_end = 0;
+        while (token_end < function.size()) {
+            auto term = terminates(function.substr(token_end, function.size() - token_end));
+            if (term != "") {
+                if (token_begin != token_end)
+                    tokenized.push_back(function.substr(token_begin, token_end - token_begin));
+
+                if (term != " ")
+                    tokenized.push_back(term);
+
+                token_end += term.size();
+                token_begin = token_end;
+            } else {
+                token_end++;
+            }
+        }
+
+        if (token_begin != token_end)
+            tokenized.push_back(function.substr(token_begin, token_end - token_begin));
+    }
+
+    /* There's all sorts of complicated options, this ignores most of them. */
+    if (tokenized.size() > 5)
+        return false;
+
+    std::vector<std::string> stripped;
+    for (const auto& token: tokenized) {
+        if (!is_all_white(token))
+            stripped.push_back(token);
+    }
+
+    return resolve_pp_function(stripped, defines);
+}
+
+static bool resolve_pp_function(
+    const std::vector<std::string>& function,
+    const std::unordered_set<std::string>& defines)
+{
+    return resolve_pp_function(function.begin(), function.end(), defines);
+}
+
+static bool resolve_pp_function(
+    std::vector<std::string>::const_iterator begin,
+    std::vector<std::string>::const_iterator end,
+    const std::unordered_set<std::string>& defines)
+{
+    static const auto pmod = [](std::string token) {
+        if (token == "(")
+            return 1;
+        if (token == ")")
+            return -1;
+        return 0;
+    };
+
+    /* Checks to see if this is a binary op. */
+    {
+        auto parens = 0;
+        auto op_index = [&](){
+            for (auto it = begin; it < end; ++it) {
+                auto token = *it;
+                parens += pmod(token);
+                if (parens != 0)
+                    continue;
+
+                if (token == "&&" || token == "||" || token == ">" || token == ">=" || token == "<" || token == "<=" || token == "==")
+                    return it;
+            }
+
+            return end;
+        }();
+
+        if (op_index != end) {
+            auto lo = [&](){ return resolve_pp_function(begin, op_index, defines); };
+            auto hi = [&](){ return resolve_pp_function(op_index + 1, end, defines); };
+            
+            if (*op_index == "&&")
+                return lo() && hi();
+            if (*op_index == "||")
+                return lo() || hi();
+
+            /* FIXME: Don't silently drop here. */
+            return false;
+        }
+    }
+
+    /* Checks to see if this is a unary op. */
+    {
+        if (*begin == "!") {
+            return !resolve_pp_function(begin + 1, end, defines);
+        }
+
+        if (*begin == "defined") {
+            return resolve_pp_defined(begin + 1, end, defines);
+        }
+
+        if (*begin == "__GNUC_PREREQ") {
+            return false;
+        }
+    }
+
+    /* This might just be a single value. */
+    {
+        if (end - begin == 1) {
+            if (*begin == "0")
+                return false;
+
+            auto f = defines.find(*begin);
+            return f != defines.end();
+        }
+    }
+
+    /* It's possible the whole thing is wrapped in a ()'s, so just strip that.
+     * */
+    {
+        if (begin[0] == "(" && end[-1] == ")")
+            return resolve_pp_function(begin + 1, end - 1, defines);
+    }
+
+    std::cerr << "Unable to parse function:\n";
+    for (auto it = begin; it < end; ++it) {
+        std::cerr << "  f: " << *it << "\n";
+    }
+    std::cerr << std::endl;
+    abort();
+    return false;
+}
+
+static bool resolve_pp_defined(
+    std::vector<std::string>::const_iterator begin,
+    std::vector<std::string>::const_iterator end,
+    const std::unordered_set<std::string>& defines)
+{
+    if ((end - begin) == 1) {
+        auto f = defines.find(*begin);
+        return f != defines.end();
+    }
+
+    if ((end - begin) == 3) {
+        if (begin[0] != "(" || begin[2] != ")") {
+            std::cerr << "Unable to parse defined, expected SYMBOL or (SYMBOL), got\n";
+            for (auto it = begin; it < end; ++it)
+                std::cerr << "  " << *it << "\n";
+            abort();
+        }
+
+        auto f = defines.find(begin[1]);
+        return f != defines.end();
+    }
+
+    std::cerr << "Unable to parse defined, expected SYMBOL or (SYMBOL), got\n";
+    for (auto it = begin; it < end; ++it)
+        std::cerr << "  " << *it << "\n";
+    abort();
+    return false;
 }

src/libpconfigure/languages/bash.c++
@@ -203,7 +203,7 @@ std::vector<makefile::target::ptr> language_bash::dependencies(const std::string
                        auto t = std::make_shared<makefile::target>(p);
                        out.push_back(t);
                        return 0;
-                   }, false);
+                   }, true);
     return out;
 }
 

src/libpconfigure/languages/cxx.c++
@@ -22,15 +22,10 @@
 #include "../language_list.h++"
 #include "../vector_util.h++"
 #include "../pick_language.h++"
-#include <pinclude.h>
+#include <pinclude.h++>
 #include <unistd.h>
 #include <fcntl.h>
 #include <iostream>
-
-struct pinclude_priv {
-    std::vector<std::string> all_files;
-};
-static int pinclude_callback(const char *filename, void *priv_uncase);
 
 language_cxx* language_cxx::clone(void) const
 {
@@ -780,46 +775,27 @@ std::vector<std::string> language_cxx::dependencies(
     const shared_target& is_shared __attribute__((unused)),
     const std::vector<std::string>& compile_opts) const
 {
-    size_t defined_i = 0;
-    const char **defined = new const char*[compile_opts.size()];
-    size_t include_dirs_i = 0;
-    const char **include_dirs = new const char*[compile_opts.size()];
+    std::vector<std::string> defined;
+    std::vector<std::string> include_dirs;
 
     for (const auto& opt: compile_opts) {
         if (strncmp(opt.c_str(), "-D", 2) == 0)
-            defined[defined_i++] = opt.c_str() + 2;
+            defined.push_back(opt.c_str() + 2);
         if (strncmp(opt.c_str(), "-I", 2) == 0)
-            include_dirs[include_dirs_i++] = opt.c_str() + 2;
+            include_dirs.push_back(opt.c_str() + 2);
     }
 
-    defined[defined_i] = nullptr;
-    include_dirs[include_dirs_i] = nullptr;
+    std::vector<std::string> all_files;
+    pinclude::list(filename,
+                   include_dirs,
+                   defined,
+                   [&](std::string s) {
+                       all_files.push_back(s);
+                       return 0;
+                   },
+                   true);
 
-    struct pinclude_priv priv;
-    pinclude_list(filename.c_str(),
-                  &pinclude_callback,
-                  &priv,
-                  &include_dirs[0],
-                  &defined[0],
-                  false);
-
-    delete[] defined;
-    delete[] include_dirs;
-
-    return priv.all_files;
-}
-
-int pinclude_callback(const char *filename, void *priv_uncast)
-{
-    struct pinclude_priv *priv = (struct pinclude_priv *)priv_uncast;
-
-    std::string filtered = filename;
-    auto dirs = std::regex("(.+)/.+/\\.\\./(.+)");
-    while (std::regex_match(filtered, dirs))
-        filtered = std::regex_replace(filtered, dirs, std::string("$1/$2"));
-
-    priv->all_files.push_back(filtered);
-    return 0;
+    return all_files;
 }
 
 static void install_cxx(void) __attribute__((constructor));

src/libpinclude/pinclude.h++
@@ -24,6 +24,7 @@
 #include <functional>
 #include <string>
 #include <vector>
+#include <unordered_set>
 
 namespace pinclude {
     /* Looks through the given file for #include lines, calling the given
@@ -34,6 +35,12 @@ namespace pinclude {
     int list(std::string filename,
              std::vector<std::string> include_dirs,
              std::vector<std::string> defined,
+             std::function<int(std::string)> callback,
+             bool skip_missing_files);
+
+    int list(std::string filename,
+             std::vector<std::string> include_dirs,
+             std::unordered_set<std::string> defined,
              std::function<int(std::string)> callback,
              bool skip_missing_files);
 

src/pinclude.c++
@@ -22,18 +22,19 @@
 #include <string>
 #include <cstring>
 #include <iostream>
+#include <unordered_set>
 
 int main(int argc, const char **argv)
 {
     std::vector<std::string> dirs;
-    std::vector<std::string> defs;
+    std::unordered_set<std::string> defs;
     std::string input;
 
     for (int i = 1; i < argc; ++i) {
         if (strncmp(argv[i], "-I", 2) == 0)
             dirs.push_back(argv[i] + 2);
         else if (strncmp(argv[i], "-D", 2) == 0)
-            dirs.push_back(argv[i] + 2);
+            defs.insert(argv[i] + 2);
         else
             input = argv[i];
     }

commit a914e83a02d8cabc3f0fdf0c946a7e67b85235b4

pconfigure: Keep the closing quote off an include's path
An #include with a comment after it lost the file it named.  The
directive parser had its own scan for the start of a comment, written
back when it ran on the raw line, but the line it sees now has already
had its comments blanked to spaces.  So the scan found nothing, ran to
the end of the line, and handed back the path with the whitespace the
comment left behind still attached.

The include parser reads the last character of that as the closing
quote.  With trailing space it reads a quote from the middle instead
and asks for a path with a quote in it, which no file has, and a
dependency goes missing without anything being said.  This one has
nothing to do with which comment syntax was used: a block comment after
an include lost it just as thoroughly as a line comment did.

Strip the trailing whitespace instead.  The comment scan can go: it has
had nothing to find since comments started being blanked out upstream.

Fixes: 024f77fa18f4 ("Implement libpinlcude++ in C++")

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

Configfiles/main
@@ -98,6 +98,7 @@ TESTSRC     += indir.bash
 TESTSRC     += existafter.bash
 TESTSRC     += comment.bash
 TESTSRC     += line-comment.bash
+TESTSRC     += trailing-comment.bash
 
 BINARIES    += pllvm-config
 SOURCES     += pllvm-config.bash

src/libpinclude/library.c++
@@ -413,17 +413,17 @@ static void check_line(const std::string& line, const std::string& pp, const std
     while (i < line.size() && isspace(line[i]))
         i++;
 
-    /* Checks to see if there's any comments, and strip those. */
-    auto comment_i = i;
-    while (comment_i < line.size()) {
-        if (line.substr(comment_i, 2) == "//")
-            break;
-        if (line.substr(comment_i, 2) == "/*")
-            break;
-        comment_i++;
-    }
+    /* By the time a line gets here its comments have already been
+     * blanked out, so looking for the start of one finds nothing and
+     * what a trailing comment leaves behind is whitespace.  That has to
+     * go: the include parser reads the last character of what it's
+     * handed as the closing quote, and trailing space makes it read a
+     * quote that is still in the middle of the string. */
+    auto end = line.size();
+    while (end > i && isspace(line[end - 1]))
+        end--;
 
-    return on_match(line.substr(i, comment_i - i));
+    return on_match(line.substr(i, end - i));
 }
 
 static bool resolve_pp_function(

test/pinclude/trailing-comment.bash
@@ -1,0 +1,2 @@
+#include "harness.bash"
+H4sIAAAAAAACA+2bz4vaQBzF0x699tjLsL3tIZnfkxz2YMvCCluwW1pob5MYV6kmEpPitX9D/7zSf6U0urBWt2hdTOI27wPDiE7QMLz38p0Z+3ZxFdtBnHlRkWVxkg/GmXNkKKVGKbLq9V1PubzrV6+pIUxxKTTVUglCmVKGOmTh1EAxz21W/pSZnUzjHbdeDhsOd99kCbnvnwg8INN8PI0vmPGNUUHAlGuoFMrIoKMMue697t68uep9vHQXNs8zN0qnrp3NJrE7y9KvcWKTKL7ovut1b2/6ofj8NvS+fOjIgLwvL7r+tOuiZ8+dby9+/Pz+8tdlxwHN4HrVf8c+/S/1sql/pqV2iIL+a5j//n0CjJNZkTcw/5xtz7/URsH/60DQv/o/F0FANQKgBf6/Un2lKXC4/wtKFfy/bv8fjielMqdxBfOvpTzE/5VgAv7fnP9Tn5fNh/+3wP+rUv2/67/8eNv/hSz9n0L/lZPH89wdQQft1f86/+f5IC1y9zadDBrOf00pQ/43WP9JThlTyP8W6L8q1R+Q/0xur/9QwZH/dWDdUScsW1Q2qKG96z/rpwB79MfBx9T/Evt/TeY/1T4LsP7bHv3bCovA/fU/f1D/a438ryX/oTzofyP/w9PIf9T/TeY/8w0XOADUHv2Hp5b/BvlfByGUB/1v5P/xd4Qes/5vDPK/ufwXWgpjUP+3R/9V7gPv0z8TD85/Mo78r4VX4ySaFIOYnFl3dEY8j1iSpHlMbJgWOVluD6yHhKsh55tDyjfJuffHqKgcBT0/2fyPTqP+58j/But/7ksW4Pxfe/QfnVj9v/z/H/K/eiIoDwAAAAAAAAAAAAAAAP5LfgM0brRZAFAAAA==